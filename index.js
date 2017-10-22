b7dc2fa6b3fbc0a3182c35fb99f393d3dc6dbd17
Alejandro Tardin <alejandro@tardin.com>
2017-10-22 23:39:39 +0200

    Removing signal handlers
    The presence of those handlers made ctrl-c not work when run from npm.
    Without them everything seems to be properly closed both from yarn and
    npm.
    
    Fixes #1

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ var bs = require('browser-sync').create();
 module.exports = function browsersync(options) {
     if (!bs.active) {
         bs.init(options || {server: '.'});
-
-        ['SIGINT', 'SIGTERM'].forEach(function(signal) {
-            process.on(signal, function() {
-                bs.exit();
-            });
-        });
     }
 
     return {
