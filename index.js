d74e100b70cc113f76a5c987d6a8db2a2105d7e2
danielbarion <danielbarionn@gmail.com>
2020-10-09 11:58:01 -0300

    fix: await bundle finish before open browser url to prevent a 404 page

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var bs = require('browser-sync').create('rollup');
 
 module.exports = function browsersync(options) {
-    if (!bs.active) {
-        bs.init(options || {server: '.'});
-    }
-
     return {
         name: 'browsersync',
-        generateBundle: function({}, bundle, isWrite) {
-            if (isWrite) {
-                bs.reload(bundle.dest);
-            }
+        writeBundle() {
+            new Promise((resolve) => {
+                /**
+                 * Only open the browser when the entire bundle
+                 * process is complete and the files are saved.
+                 */
+                if (!bs.active) {
+                    bs.init(options || { server: '.' })
+                }
+
+                resolve()
+            })
         }
     }
 };
