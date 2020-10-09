1f2265b610532e848d414c124c4c4f1f13fdc4ff
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-10-09 22:31:03 +0200

    Initializing browsersync on first write

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var bs = require('browser-sync').create('rollup');
 
-module.exports = function browsersync(options) {
-    if (!bs.active) {
-        bs.init(options || {server: '.'});
-    }
-
+module.exports = function browsersync(browserSyncOptions) {
     return {
         name: 'browsersync',
         writeBundle: function(options) {
-            bs.reload(options.file);
+            if (!bs.active) {
+                bs.init(browserSyncOptions || {server: '.'});
+            } else {
+                bs.reload(options.file);
+            }
         }
     }
 };
